1cea4f54206f985a1e6755694a26ae2d246c7727
Merlinkk <anshulkashyap2504@gmail.com>
2023-10-25 14:04:28 +0530

    background position right

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const add = document.querySelectorAll('.addtocart')
 const cartItems = document.getElementById('cartItems')
 
 add.forEach(function(add){
-    add.addEventListener('click', function(){
+    add.addEventListener('click', function(e){
+        console.log(e)
         var currentItems = parseInt(cartItems.innerText, 10);
         cartItems.innerText = ++currentItems
     })
