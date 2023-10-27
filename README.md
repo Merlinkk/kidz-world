67534f3210d4a240e7b9dd6b2af302aff9201a4f
Merlinkk <anshulkashyap2504@gmail.com>
2023-10-27 14:11:32 +0530

    cart added

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ## Kidz world is an Online Shopping Websites for kids to discover popular books made for kids
 
 ### The website offers a collection of books, games and handicrafts.
-#### Click on Add to add items to cart and click on cart to view added items
+#### Click on Add to add items to cart and click on cart to view added items in console
 
 ### Done by Anshul Kashyap
--- script.js
+++ script.js
@@ -4,78 +4,93 @@ const cartItems = document.getElementById('cartItems')
 const items = [{
     title: "This was our pact",
     author: "Ryan Andrews",
+    imgurl: "./assets/book1.png",
     cost: 7.49
+    
 },
 {
     title: "The Famous Five",
     author: "Guid Blyton",
+    imgurl: "./assets/book2.png",
     cost: 4.59
 },
 {
     title: "Matlida",
     author: "Roald Dahi",
+    imgurl: "./assets/book3.png",
     cost: 6.80
 },
 {
     title: "Harry Potter",
     author: "JK Rowling",
+    imgurl: "./assets/book4.png",
     cost: 10
 },
 {
     title: "For Young Readers",
     author: "Ruskin Bond",
+    imgurl: "./assets/book5.png",
     cost: 7.29
 },
 {
     title: "Wimpy Kid - DIY",
     author: "Jeff Kinney",
+    imgurl: "./assets/book6.png",
     cost: 4.99
 },
 {
     title: "Dart Board",
     author: "Wooden Board, 16 inches",
+    imgurl: "./assets/dart-board.png",
     cost: 17.49
 },
 
 {
     title: "Connect 4",
     author: "Board game, multiplayer",
+    imgurl: "./assets/connect-four.png",
     cost: 19.99
 },
 
 {
     title: "Jenga",
     author: "Wooden Blocks, 54 pieces",
+    imgurl: "./assets/jenga.png",
     cost: 20.99
 },
 
 {
     title: "Monopoly",
     author: "1.61 x 15.75 x 10.51 inches",
+    imgurl: "./assets/monopoly.png",
     cost:19.49
 },
 
 {
     title: "Bookmarks",
     author: "Handmade with love",
+    imgurl: "./assets/bookmarks.png",
     cost: 12.49
 },
 
 {
     title: "Birthday Card",
     author: "Handmade with love",
+    imgurl: "./assets/birthday-card.png",
     cost: 19.99
 },
 
 {
     title: "Stuffed toys",
     author: "Handmade with love",
+    imgurl: "./assets/stuffed-toy.png",
     cost: 15.99
 },
 
 {
     title: "Dream Catcher Drawing",
     author: "Handmade with love",
+    imgurl: "./assets/dream-catcher.png",
     cost:18.49
 }
 
@@ -96,6 +111,7 @@ add.forEach(function(element,index){
 
         const itemName = items[index].title
         const itemPrice = items[index].cost
+        const imglink = items[index].imgurl
 
         const isItemInCart = addToCartItems.some((el) => {
             if (el.name === itemName) {
@@ -106,7 +122,7 @@ add.forEach(function(element,index){
           });
 
         if (!isItemInCart) {
-            addToCartItems.push({ name: itemName, price: itemPrice, quantity: 1 });
+            addToCartItems.push({ name: itemName, price: itemPrice, objIndex:index, link:imglink, quantity: 1 });
     }
 
 
@@ -118,17 +134,48 @@ const cartdiv = document.getElementById('cartdiv')
 
 var totalCost = 0
 
-// addToCartItems.forEach((ele)=>{
-    
-// })
+// popup item list
 
+const OrderList = document.getElementById('OrderList')
+const costH = document.getElementById('costDisplay')
+
+// list entry function
+function entry(elem){
+    OrderList.innerHTML += `<li> <div class="listItem"> <img src="${elem.link}" height="90%"></div> <div class="cardtext">
+    <h3>${elem.name}</h3> <br> <h4>Quantity - ${elem.quantity}</h4>
+</div>`
+}
+
+function clearList(){
+    OrderList.innerHTML = ""
+}
+
+
+
+function entryCost(dollar,cents){
+    costH.innerHTML = `The total amount is $${dollar} and ${cents} cents.`
+}
+
+// cart eventlistner
 
 cartdiv.onclick = () =>{
+    // for popup and blur
+    infoDiv.style.display = "block"
+    blur.style.filter = 'blur(10px)'
+    blur.style.zIndex = '-99' 
+    // popup and blur  
+
+    // disable scroll for page
+    document.body.style.overflow = "hidden"
+    // 
+    clearList()
     var itemsOrdered = ""
     totalCost = 0
     addToCartItems.forEach((elem)=>{
         itemsOrdered += `Item Name : ${elem.name} - Quantity: ${elem.quantity} \n`
         totalCost+= elem.price * elem.quantity
+        entry(elem)
+        
     })
     // console.log(totalCost)
     
@@ -137,4 +184,36 @@ cartdiv.onclick = () =>{
     let cents = Math.round((totalCost-dollars)*100)
     console.log(itemsOrdered)
     console.log(`The total amount is $${dollars} and ${cents} cents.`)
+    entryCost(dollars,cents)
 }
+
+// popup code
+
+const cross = document.getElementById('cross')
+const infoDiv = document.getElementById('popup')
+const blur = document.getElementById('blur')
+infoDiv.style.display = 'none'
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      onEscapeKeyPressed();
+    }
+  });
+
+function onEscapeKeyPressed(){
+    popup.style.display = 'none'
+    blur.style.filter = ''
+    blur.style.zIndex = '' 
+    document.body.style.overflow = "scroll"
+    
+}   
+cross.onclick = () =>{
+    popup.style.display = 'none'
+    blur.style.filter = ''
+    blur.style.zIndex = '' 
+    document.body.style.overflow = "scroll"
+
+}
+
+// 
+
